2466e1882343e7654ee675ec40740bf31ebc840d
Per Kristian Kummermo <per.kristian@telenordigital.com>
2017-09-01 11:27:32 +0200

    feat(dev): add simple dev environment

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     docName: "Documentation Scaffold",
     webRoot: "/docs-scaffold/",
+    webrootDev: "/",
     targetDir: "build", // relative to the project root, do not use
     editUrl: "https://github.com/TelenorFrontend/docs-scaffold/edit/master/" // empty string will disable edit button
-}
+};
